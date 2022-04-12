5ce1284502422b2a373ca30e769e72120c42ead2
Bhushan Borole <borolebhushan8@gmail.com>
2022-04-12 17:33:33 +0100

    6

--- src/components/RegisterPage/RegisterService.js
+++ src/components/RegisterPage/RegisterService.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
-import { Button, Form, FormGroup, Label, Input, Container } from "reactstrap";
-import { signupUser } from "../../actions/userActions";
 import Role from "../../helpers/role";
-import { useNavigate } from "react-router-dom";
+import {
+  Globalstyle,
+  StyleForm,
+  StyleFormedWrapper,
+  StyledButton,
+  StyledInput,
+} from "../styled-component/FormStyle";
+import axios from "axios";
+import API_URL from "../../environment";
 
 export default function RegisterService() {
-  const navigate = useNavigate();
   const [enteredName, setEnteredName] = useState("");
   const [enteredSurname, setEnteredSurname] = useState("");
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword1, setEnteredPassword1] = useState("");
   const [enteredPassword2, setEnteredPassword2] = useState("");
+  const [hasSignedIn, setHasSignedIn] = useState(false);
+  const [error, setIsError] = useState(false);
 
   const nameChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -35,74 +42,103 @@ export default function RegisterService() {
       name: enteredName,
       surname: enteredSurname,
       email: enteredEmail,
-      password: enteredPassword1,
-      confirmPassword: enteredPassword2,
-      role: Role.Admin,
+      password1: enteredPassword1,
+      password2: enteredPassword2,
+      role: Role.EmergencyStaff,
     };
-    signupUser(signUpData);
-    navigate("/login");
+    signUpUser(signUpData);
   };
 
+  const signUpUser = async (user) => {
+    axios.post(`${API_URL}/api/users/signup`, user)
+      .then(res => {
+        if(res.status===200) {
+          setHasSignedIn(true);
+        }
+      })
+      .catch((err)=>{
+        setIsError(true)
+        console.log(err);
+      })
+  }
+
   return (
-    <Container>
-      <Form onSubmit={submitHandler}>
-        <FormGroup>
-          <Label for="userName">Name</Label>
-          <Input
-            type="text"
+    <>
+      <Globalstyle />
+      <StyleFormedWrapper>
+        <StyleForm onSubmit={submitHandler}>
+          <div>
+            <label htmlFor="userName">Name</label>
+            <StyledInput
+              type="text"
             name="name"
             id="userName"
             onChange={nameChangeHandler}
             value={enteredName}
             placeholder="Enter your Name"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="userSurname">Surname</Label>
-          <Input
-            type="text"
+            />
+          </div>
+          <div>
+            <label htmlFor="userSurname">Surname</label>
+            <StyledInput
+              type="text"
             name="surname"
             id="userSurname"
             onChange={surnameChangeHandler}
             value={enteredSurname}
             placeholder="Enter your Surname"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="userEMail">Email</Label>
-          <Input
-            type="email"
+            />
+          </div>
+          <div>
+            <label htmlFor="userEMail">Email</label>
+            <StyledInput
+              type="email"
             name="email"
             id="userEMail"
             onChange={emailChangeHandler}
             value={enteredEmail}
             placeholder="Enter your Email"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="userPassword1">Enter your Password</Label>
-          <Input
-            type="password"
+            />
+          </div>
+          <div>
+            <label htmlFor="userPassword1">Password</label>
+            <StyledInput
+              type="password"
             name="password1"
             id="userPassword1"
             onChange={password1ChangeHandler}
             value={enteredPassword1}
             placeholder="Enter your Password"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="userPassword2">Confirm your Password</Label>
-          <Input
-            type="password"
+            />
+          </div>
+          <div>
+            <label htmlFor="userPassword2">Confirm Pasword</label>
+            <StyledInput
+              type="password"
             name="password2"
             id="userPassword2"
             onChange={password2ChangeHandler}
             value={enteredPassword2}
             placeholder="Enter your Password Again"
-          />
-        </FormGroup>
-        <Button type="Submit">Sign Up</Button>
-      </Form>
-    </Container>
+            />
+          </div>
+          <div>
+            <StyledButton type="submit">Register</StyledButton>
+          </div>
+          <div>
+            {hasSignedIn && (
+              <h4 style={{ color: "green", textAlign: "center" }}>
+                  User Created!.
+              </h4>
+            )}
+            {error && (
+              <h4 style={{ color: "red", textAlign: "center" }}>
+                  Failed!.
+              </h4>
+            )}
+          </div>
+        </StyleForm>
+      </StyleFormedWrapper>
+    </>
   );
 }
